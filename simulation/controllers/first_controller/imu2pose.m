190fa28f77f012f85ca4341363da007c151a14cd
Dong-Jae Park <dj4park@gmail.com>
2021-01-06 13:33:05 -0600

    Function name change
    imu2pose -> imu_acc2vel

--- simulation/controllers/first_controller/imu2pose.m
+++
@@ -1,6 +0,0 @@
-function [x, z] = imu2pose(acc_x, acc_z, vel_x, vel_z, prev_x, prev_z)
-    %imu2pose: Convert IMU values to robot pose 
-    
-    
-end
-
---
+++ simulation/controllers/first_controller/imu_acc2vel.m
@@ -0,0 +1,6 @@
+function [x, z] = imu_acc2vel(acc_x, acc_z)
+    %imu2pose: Convert IMU values to robot pose 
+    
+    
+end
+
