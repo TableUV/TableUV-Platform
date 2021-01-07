95be4c9cc4ab4e30a291a3c6d9ab3bf7863215df
Dong-Jae Park <dj4park@gmail.com>
2021-01-06 21:24:52 -0600

    Fixed velocity calculation from IMU acceleration

--- simulation/controllers/first_controller/imu_acc2vel.m
+++ simulation/controllers/first_controller/imu_acc2vel.m
@@ -1,6 +1,6 @@
-function [x, z] = imu_acc2vel(acc_x, acc_z)
-    %imu2pose: Convert IMU values to robot pose 
+function vel_z = imu_acc2vel(acc_z, prev_vel_z, TIME_STEP)
+    dt = TIME_STEP * 0.001;
     
-    
+    vel_z = prev_vel_z + acc_z*dt;
 end
 
