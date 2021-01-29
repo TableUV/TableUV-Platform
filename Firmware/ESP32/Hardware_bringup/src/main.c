4e5b68918b4337258d450d355d955a3e45a8910d
Tsugumi Murata <tmurata@uwaterloo.ca>
2021-01-29 01:06:28 -0500

    add blink code

---
+++ Firmware/ESP32/Hardware_bringup/src/main.c
@@ -0,0 +1,74 @@
+/*
+#####################################################################
+#####################################################################
+                    PROJECT: TABLE UV 
+#####################################################################
+#####################################################################
+
+#####################################################################
+                    BOARD    : ESP32-WROOM-32U
+                    PROGRAM  : Hardware_Bringup Code
+                    DEVELOPER: Tsugumi Murata (github: tsuguminn0401)
+                    DESCRIPTION: General code to test ESP32 IO and peripherals 
+#####################################################################
+
+
+*/
+// Standard libraries 
+#include <string.h>
+#include <stdio.h>
+
+// Driver Libraries 
+#include "driver/gpio.h"
+#include "driver/rtc_io.h"
+#include "driver/adc.h"
+#include "driver/dac.h"
+
+// SDK config 
+#include "sdkconfig.h"
+
+// Non-Volatile-Storage 
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include "soc/rtc_cntl_reg.h"
+#include "soc/sens_reg.h"
+#include "soc/rtc_periph.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp32/ulp.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+// GPIO MACRO 
+#define STATUS_RED_LED       13
+#define STATUS_GREEN_LED     12
+
+void app_main()
+{
+  
+    gpio_pad_select_gpio(STATUS_RED_LED);
+    gpio_pad_select_gpio(STATUS_GREEN_LED);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(STATUS_RED_LED, GPIO_MODE_OUTPUT);
+    gpio_set_direction(STATUS_GREEN_LED, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+	printf("Turning off the LED\n");
+        gpio_set_level(STATUS_RED_LED, 0);
+        gpio_set_level(STATUS_GREEN_LED, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        /* Blink on (output high) */
+	printf("Turning on the LED\n");
+        gpio_set_level(STATUS_RED_LED, 1);
+        gpio_set_level(STATUS_GREEN_LED, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
