fb54022a6326e5e6064e6462840cf5caf0366ff6
Jack <projectbyjx@gmail.com>
2021-02-15 22:05:55 -0500

    + freertos update @ 1hz 10hz 100hz

--- Firmware/ESP32/Hardware_bringup/src/main.c
+++ Firmware/ESP32/Hardware_bringup/src/main.c
@@ -17,24 +17,155 @@
 // SDK config 
 #include "sdkconfig.h"
 
-// for pwm setting 
-#define PWM_CH_NUM        2
-#define PWM_FREQUENCY     1000
-#define PWM_RESOLUTION    LEDC_TIMER_13_BIT
-#define PWM_SPEED_MODE    LEDC_HIGH_SPEED_MODE
-#define PWM_TIMER_NUM     LEDC_TIMER_0
+// FreeRTOS
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-typedef enum {
-    UVDRIVER_ARRAY_BOARD= 0, /*!< LEDC high speed speed_mode */
-    UVDRIVER_SINGLE_BOARD,      /*!< LEDC low speed speed_mode */
-} uv_driver_board_t;
+/////////////////////////////////
+///////   DEFINITION     ////////
+/////////////////////////////////
+#define ESP32_CORE_LOW_LEVEL    (0U)
+#define ESP32_CORE_HIGH_LEVEL   (1U)
 
+#define TASK_10MS               (10 / portTICK_PERIOD_MS)
+#define TASK_100MS              (100 / portTICK_PERIOD_MS)
+#define TASK_500MS              (500 / portTICK_PERIOD_MS)
+#define TASK_1000MS             (1000 / portTICK_PERIOD_MS)
+
+/////////////////////////////////////////
+///////   PRIVATE PROTOTYPE     /////////
+/////////////////////////////////////////
+static void esp32_task_init(void);
+
+static void core0_task_run10ms(void * pvParameters);
+static void core0_task_run100ms(void * pvParameters);
+static void core0_task_run1000ms(void * pvParameters);
+static void core1_task_run500ms(void * pvParameters);
+
+///////////////////////////
+///////   DATA     ////////
+///////////////////////////
+
+
+////////////////////////////////////////
+///////   PRIVATE FUNCTION     /////////
+////////////////////////////////////////
+static void core0_task_run10ms(void * pvParameters)
+{
+    /* Block for 10ms. */
+    const TickType_t xDelay = TASK_10MS;
+
+    for( ;; )
+    {
+        /* Do sth at */
+        {
+            //  add task
+            dev_run10ms();
+        }
+        vTaskDelay( xDelay );
+    }
+}
+static void core0_task_run100ms(void * pvParameters)
+{
+    /* Block for 100ms. */
+    const TickType_t xDelay = TASK_100MS;
+
+    for( ;; )
+    {
+        /* Do sth at */
+        {
+            //  add task
+            dev_run100ms();
+        }
+        vTaskDelay( xDelay );
+    }
+}
+static void core0_task_run1000ms(void * pvParameters)
+{
+    /* Block for 1000ms. */
+    const TickType_t xDelay = TASK_1000MS;
+
+    for( ;; )
+    {
+        /* Do sth at */
+        {
+            //  add task
+            dev_run1000ms();
+        }
+        vTaskDelay( xDelay );
+    }
+}
+
+static void core1_task_run500ms(void * pvParameters)
+{
+    // 20 Hz Update
+    /* Block for 500ms. */
+    const TickType_t xDelay = TASK_500MS;
+
+    for( ;; )
+    {
+        /* Do sth at */
+        {
+            //  add task (High Level)
+        }
+        vTaskDelay( xDelay );
+    }
+}
+
+static void esp32_task_init()
+{
+    // Low Level Core Init.
+    xTaskCreatePinnedToCore(
+        core0_task_run10ms,     /* Function to implement the task */
+        "core0_task_run10ms",   /* Name of the task */
+        10000,                  /* Stack size in words */
+        NULL,                   /* Task input parameter */
+        0,                      /* Priority of the task */
+        NULL,                   /* Task handle. */
+        ESP32_CORE_LOW_LEVEL    /* Core where the task should run */
+    );  
+    xTaskCreatePinnedToCore(
+        core0_task_run100ms,    /* Function to implement the task */
+        "core0_task_run100ms",  /* Name of the task */
+        10000,                  /* Stack size in words */
+        NULL,                   /* Task input parameter */
+        0,                      /* Priority of the task */
+        NULL,                   /* Task handle. */
+        ESP32_CORE_LOW_LEVEL    /* Core where the task should run */
+    );  
+    xTaskCreatePinnedToCore(
+        core0_task_run1000ms,   /* Function to implement the task */
+        "core0_task_run1000ms", /* Name of the task */
+        10000,                  /* Stack size in words */
+        NULL,                   /* Task input parameter */
+        0,                      /* Priority of the task */
+        NULL,                   /* Task handle. */
+        ESP32_CORE_LOW_LEVEL    /* Core where the task should run */
+    );  
+
+    //  High Level Core Init.
+    xTaskCreatePinnedToCore(
+        core1_task_run500ms,    /* Function to implement the task */
+        "core1_task_run500ms",  /* Name of the task */
+        10000,                  /* Stack size in words */
+        NULL,                   /* Task input parameter */
+        0,                      /* Priority of the task */
+        NULL,                   /* Task handle. */
+        ESP32_CORE_HIGH_LEVEL   /* Core where the task should run */
+    );  
+}
+
+///////////////////////////////////////
+///////   PUBLIC FUNCTION     /////////
+///////////////////////////////////////
 void app_main()
 {
     // device initialization
     dev_init();
 
-    while(1) {
+    // esp32 task initialization
+    esp32_task_init();
 
-    }
+    // forever loop
+    while (true){};
 }
