a049ff7d742c36a3a8c41a71fd19e14ae9d51ca3
Tsugumi Murata <tmurata@uwaterloo.ca>
2021-02-18 16:17:58 -0500

    init commit for sensor avr

---
+++ Firmware/AVR/AVR_SENSOR/src/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author 
+ * @date 18 Feb 2021
+ * @brief Main File for AVR SENSOR
+ *
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>    
+#include <util/delay.h>      
+#include <stdint.h>  
+
+int main(){
+  
+}
