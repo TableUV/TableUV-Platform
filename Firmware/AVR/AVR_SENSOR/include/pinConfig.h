4843dfc4eb795773dbc1d0c0abf57f51c22eccc5
Jerome <jvillapando@ne-rv.com>
2021-02-18 17:52:24 -0500

    created structure

---
+++ Firmware/AVR/AVR_SENSOR/include/pinConfig.h
@@ -0,0 +1,40 @@
+/**
+ * @file pinConfig.h
+ * @author Tsugumi Murata (tmurata293)
+ * @date 17 Feb 2021
+ * @brief Pin Config File for AVR DRIVER
+ *
+ */
+
+
+#ifndef _PINCONFIG_H_
+#define _PINCONFIG_H_
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#define PIN_DIR_INPUT       0
+#define PIN_DIR_OUTPUT      1
+
+#define RIGHT_COLLISION     PB0     //output, OC0A
+#define LEFT_COLLISION      PB1     //output, OC0B
+
+#define IR_FRONT_1          PA7
+#define IR_FRONT_2          PA6
+#define IR_RIGHT_1          PA4
+#define IR_RIGHT_2          PA5
+#define IR_LEFT_1           PA0
+#define IR_LEFT_2           PA3
+
+#define UART_TX             PA2
+#define UART_RX             PA1                          
+
+#ifdef __cplusplus  
+}
+#endif 
+
+
+#endif 
---
+++ Firmware/AVR/AVR_SENSOR/lib/collision/collision.h
@@ -0,0 +1,17 @@
+/**
+ * @file collision.h
+ * @author Jerome Villapando
+ * @date 15 Feb 2021
+ * @brief Device configure header files
+ *
+ * This document will contains device configure content
+ */
+
+
+#ifndef COLLISION_H
+#define COLLISION_H
+
+void collision_init(void);
+
+
+#endif //DEV_BATTERY_H
