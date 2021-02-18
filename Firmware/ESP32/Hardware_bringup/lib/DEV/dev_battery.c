522651310d024463ba610d8bf7527b6a52f3ff37
Jerome <jvillapando@ne-rv.com>
2021-02-17 22:20:17 -0500

    Implemented functions for battery voltage adc

--- Firmware/ESP32/Hardware_bringup/lib/DEV/dev_battery.c
+++ Firmware/ESP32/Hardware_bringup/lib/DEV/dev_battery.c
@@ -9,16 +9,23 @@
 
 #include "dev_battery.h"
 
+// Standard libraries 
+#include <stdint.h>
+
 // TableUV Lib
 #include "../IO/io_ping_map.h"
 
 // External Lib
 #include "driver/gpio.h"
+#include "driver/adc.h"
 
 /////////////////////////////////
 ///////   DEFINITION     ////////
 /////////////////////////////////
-
+typedef struct{
+    float battery_voltage;
+    int32_t battery_voltage_raw;
+} dev_battery_data_S;
 
 /////////////////////////////////////////
 ///////   PRIVATE PROTOTYPE     /////////
@@ -29,7 +36,7 @@ static inline void dev_battery_private_gpio_config(void);
 ///////////////////////////
 ///////   DATA     ////////
 ///////////////////////////
-
+static dev_battery_data_S battery_data;
 
 ////////////////////////////////////////
 ///////   PRIVATE FUNCTION     /////////
@@ -43,11 +50,7 @@ static inline void dev_battery_private_gpio_config(void)
     gpio_config_t io_conf;
     //gpio config for input 
     io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask =  (1ULL<< CHARGE_STATUS); 
-    gpio_config(&io_conf);
-
-    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    io_conf.pin_bit_mask = (1ULL<< BATTERY_VOLTAGE);
+    io_conf.pin_bit_mask =  ((1ULL<< CHARGE_STATUS) | (1ULL<< BATTERY_VOLTAGE)); 
     gpio_config(&io_conf);
 }
 
@@ -57,5 +60,41 @@ static inline void dev_battery_private_gpio_config(void)
 void dev_battery_init(void)
 {
     dev_battery_private_gpio_config();
+    adc2_config_channel_atten( ADC2_CHANNEL_2, ADC_ATTEN_DB_0 );
 }
 
+float dev_battery_convert_to_lipo_volts(int32_t raw_adc)
+{
+    return raw_adc * 0.8/4096 * (BATTERY_PULLUP_KOHMS + BATTERY_PULLDOWN_KOHMS) / BATTERY_PULLDOWN_KOHMS;
+}
+
+void dev_battery_update(void)
+{
+    esp_err_t r = adc2_get_raw( ADC2_CHANNEL_2, ADC_WIDTH_12Bit, &battery_data.battery_voltage_raw);
+    if ( r == ESP_ERR_TIMEOUT ) 
+    {
+        printf("ADC2 used by Wi-Fi.\n");
+    }
+
+    battery_data.battery_voltage = dev_battery_convert_to_lipo_volts(battery_data.battery_voltage_raw);
+
+}
+
+float dev_battery_get(void)
+{
+    return battery_data.battery_voltage;
+}
+
+// Output: 0-4096
+int32_t dev_battery_read_raw(void)
+{
+    int32_t raw_voltage = 0;
+
+    esp_err_t r = adc2_get_raw( ADC2_CHANNEL_2, ADC_WIDTH_12Bit, &raw_voltage);
+    if ( r == ESP_ERR_TIMEOUT ) 
+    {
+        printf("ADC2 used by Wi-Fi.\n");
+    }
+
+    return raw_voltage;
+}
--- Firmware/ESP32/Hardware_bringup/lib/DEV/dev_battery.h
+++ Firmware/ESP32/Hardware_bringup/lib/DEV/dev_battery.h
@@ -11,7 +11,16 @@
 #ifndef DEV_BATTERY_H
 #define DEV_BATTERY_H
 
+#include <stdint.h>
+
+#define BATTERY_PULLUP_KOHMS    249
+#define BATTERY_PULLDOWN_KOHMS  16.9
+
 void dev_battery_init(void);
+float dev_battery_convert_to_lipo_volts(int32_t raw_adc);
+void dev_battery_update(void);
+float dev_battery_get(void);
+int32_t dev_battery_read_raw(void);
 
 
 #endif //DEV_BATTERY_H
