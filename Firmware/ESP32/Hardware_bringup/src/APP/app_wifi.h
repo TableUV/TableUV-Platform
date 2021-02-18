dd774297839445d148bcbedafde677e5f9ed3e1d
Dong-Jae Park <dj4park@gmail.com>
2021-02-17 20:59:47 -0500

    + app_wifi

---
+++ Firmware/ESP32/Hardware_bringup/src/APP/app_wifi.h
@@ -0,0 +1,15 @@
+/**
+ * @file    app_wifi.h
+ * @author  Dong-Jae (Alex) Park
+ * @date    17 Feb 2021
+ * @brief   Wifi initialization with router
+ * 
+ * Wifi initialization with router
+ */
+
+#ifndef APP_WIFI_H
+#define APP_WIFI_H
+
+void app_wifi_init(void);
+
+#endif //APP_WIFI_H
