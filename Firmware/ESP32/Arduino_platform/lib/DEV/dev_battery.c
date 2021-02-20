ab2f9a6c89cc9a06df4b42227c884c80a8c28810
Jerome <jvillapando@ne-rv.com>
2021-02-19 22:08:44 -0500

    brought old esp code to arduino platform

--- Firmware/ESP32/Arduino_platform/lib/DEV/dev_battery.c
+++ Firmware/ESP32/Arduino_platform/lib/DEV/dev_battery.c
@@ -1,6 +1,6 @@
 /**
  * @file dev_battery.c
- * @author Jianxiang (Jack) Xu
+ * @author Jerome Villapando
  * @date 15 Feb 2021
  * @brief Device configure files
  *
@@ -9,16 +9,19 @@
 
 #include "dev_battery.h"
 
-// TableUV Lib
-#include "../IO/io_ping_map.h"
-
-// External Lib
-#include "driver/gpio.h"
-
 /////////////////////////////////
 ///////   DEFINITION     ////////
 /////////////////////////////////
-
+typedef enum {
+    CHARGING,                   // STAT Pin Low
+    CHARGE_COMPLETE_SLEEP,      // STAT Pin HIGH
+    CHARGER_FAULT               // STAT Pin Blinkings
+} charger_ic_status_t;
+typedef struct {
+    float battery_voltage;              // Volts
+    int32_t battery_voltage_raw;        // 0 - 4096
+    charger_ic_status_t charger_status;
+} dev_battery_data_S;
 
 /////////////////////////////////////////
 ///////   PRIVATE PROTOTYPE     /////////
@@ -29,11 +32,17 @@ static inline void dev_battery_private_gpio_config(void);
 ///////////////////////////
 ///////   DATA     ////////
 ///////////////////////////
-
+static dev_battery_data_S battery_data;
+static volatile bool edge_detected = 0;
 
 ////////////////////////////////////////
 ///////   PRIVATE FUNCTION     /////////
 ////////////////////////////////////////
+static void IRAM_ATTR charger_fault_isr_handler(void* arg)
+{
+    edge_detected = 1;
+}
+
 static inline void dev_battery_private_gpio_config(void)
 {
     //IO mode select  
@@ -43,8 +52,16 @@ static inline void dev_battery_private_gpio_config(void)
     gpio_config_t io_conf;
     //gpio config for input 
     io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask =  ((1ULL<< CHARGE_STATUS)|(1ULL<< BATTERY_VOLTAGE)) ; 
+    io_conf.pin_bit_mask =  (1ULL<< BATTERY_VOLTAGE); 
     gpio_config(&io_conf);
+
+    io_conf.pin_bit_mask = (1ULL<< CHARGE_STATUS);
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(CHARGE_STATUS, charger_fault_isr_handler, (void*)CHARGE_STATUS);
+    gpio_intr_disable(CHARGE_STATUS);
 }
 
 ///////////////////////////////////////
@@ -53,5 +70,88 @@ static inline void dev_battery_private_gpio_config(void)
 void dev_battery_init(void)
 {
     dev_battery_private_gpio_config();
+    adc2_config_channel_atten( ADC2_CHANNEL_2, ADC_ATTEN_DB_0 );
 }
 
+float dev_battery_convert_to_lipo_volts(int32_t raw_adc)
+{
+    return raw_adc * 0.8/4096 * (BATTERY_PULLUP_KOHMS + BATTERY_PULLDOWN_KOHMS) / BATTERY_PULLDOWN_KOHMS;
+}
+
+void dev_battery_update(void)
+{
+    esp_err_t r = adc2_get_raw( ADC2_CHANNEL_2, ADC_WIDTH_12Bit, &battery_data.battery_voltage_raw);
+    if ( r == ESP_ERR_TIMEOUT ) 
+    {
+        printf("ADC2 used by Wi-Fi.\n");
+    }
+
+    battery_data.battery_voltage = dev_battery_convert_to_lipo_volts(battery_data.battery_voltage_raw);
+
+}
+
+float dev_battery_get(void)
+{
+    return battery_data.battery_voltage;
+}
+
+// Output: 0-4096
+int32_t dev_battery_read_raw(void)
+{
+    int32_t raw_voltage = 0;
+
+    esp_err_t r = adc2_get_raw( ADC2_CHANNEL_2, ADC_WIDTH_12Bit, &raw_voltage);
+    if ( r == ESP_ERR_TIMEOUT ) 
+    {
+        printf("ADC2 used by Wi-Fi.\n");
+    }
+
+    return raw_voltage;
+}
+
+void dev_charger_status_update(void)
+{
+    edge_detected = 0;
+    gpio_intr_enable(CHARGE_STATUS);
+    vTaskDelay(2000 / portTICK_RATE_MS);
+    gpio_intr_disable(CHARGE_STATUS);
+    
+    if(edge_detected)
+    {
+        battery_data.charger_status = CHARGER_FAULT;
+    }
+    else if(gpio_get_level(CHARGE_STATUS))
+    {
+        battery_data.charger_status = CHARGE_COMPLETE_SLEEP;
+    }
+    else
+    {
+        battery_data.charger_status = CHARGING;
+    }
+}
+
+int8_t dev_charger_status_get(void)
+{
+    return battery_data.charger_status;
+}
+
+int8_t dev_charger_status_read(void)
+{
+    edge_detected = 0;
+    gpio_intr_enable(CHARGE_STATUS);
+    vTaskDelay(2000 / portTICK_RATE_MS);
+    gpio_intr_disable(CHARGE_STATUS);
+
+    if(edge_detected)
+    {
+        return CHARGER_FAULT;
+    }
+    else if(gpio_get_level(CHARGE_STATUS))
+    {
+        return CHARGE_COMPLETE_SLEEP;
+    }
+    else
+    {
+        return CHARGING;
+    }
+}
--- Firmware/ESP32/Arduino_platform/lib/DEV/dev_battery.h
+++ Firmware/ESP32/Arduino_platform/lib/DEV/dev_battery.h
@@ -1,22 +1,39 @@
 /**
  * @file dev_battery.h
- * @author Jianxiang (Jack) Xu
+ * @author Jerome Villapando
  * @date 15 Feb 2021
  * @brief Device configure header files
  *
  * This document will contains device configure content
  */
 
-
 #ifndef DEV_BATTERY_H
 #define DEV_BATTERY_H
-# ifdef __cplusplus
-extern "C"{
-# endif 
 
+// Standard libraries 
+#include <stdint.h>
+
+// TableUV Lib
+#include "../IO/io_ping_map.h"
+
+// External Lib
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define BATTERY_PULLUP_KOHMS    249
+#define BATTERY_PULLDOWN_KOHMS  16.9
+
+// Function Prototypes
 void dev_battery_init(void);
+float dev_battery_convert_to_lipo_volts(int32_t raw_adc);
+void dev_battery_update(void);
+float dev_battery_get(void);
+int32_t dev_battery_read_raw(void);
+void dev_charger_status_update(void);
+int8_t dev_charger_status_get(void);
+int8_t dev_charger_status_read(void);
 
-# ifdef __cplusplus  
-}
-# endif 
+
 #endif //DEV_BATTERY_H
