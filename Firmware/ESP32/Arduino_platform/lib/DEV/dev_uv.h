e370dce546c020ce1d16e9af2f78e3592dfe66c3
Dong-Jae Park <dj4park@gmail.com>
2021-02-23 15:16:09 -0500

    + uv configuration

--- Firmware/ESP32/Arduino_platform/lib/DEV/dev_uv.h
+++ Firmware/ESP32/Arduino_platform/lib/DEV/dev_uv.h
@@ -4,7 +4,7 @@
  * @date 15 Feb 2021
  * @brief UV LED Drivers
  *
- * This document will contains device uv content
+ * This document contains implementations for UV
  */
 
 
@@ -12,9 +12,59 @@
 #define DEV_UV_H
 # ifdef __cplusplus
 extern "C"{
-# endif 
+# endif
 
+#include <stdint.h>
+#include <stdbool.h>
+
+/////////////////////////////////
+///////   DEFINITION     ////////
+/////////////////////////////////
+typedef enum {
+    DEV_UV_LED_ROW,
+    DEV_UV_LED_SIDE,
+    DEV_UV_LED_COUNT,
+    DEV_UV_LED_UNKNOWN
+} DEV_UV_E;
+
+///////////////////////////////////////
+///////   PUBLIC PROTOTYPE    /////////
+///////////////////////////////////////
+
+/**
+ * @brief This function initializes the 
+ */
 void dev_uv_init(void);
+
+/**
+ * @brief This function sets both row and side UV LEDs on.
+ * 
+ * @param pwn_duty: buffer pointer used to copy over the data
+ * @param dac_duty: buffer pointer used to copy over the data
+ */
+void dev_uv_set_both(int pwm_duty, uint8_t dac_duty);
+
+/**
+ * @brief This function sets row UV LEDs on.
+ * 
+ * @param pwn_duty: buffer pointer used to copy over the data
+ * @param dac_duty: buffer pointer used to copy over the data
+ */
+void dev_uv_set_row(int pwm_duty, uint8_t dac_duty);
+
+/**
+ * @brief This function sets side UV LEDs on.
+ * 
+ * @param pwn_duty: buffer pointer used to copy over the data
+ * @param dac_duty: buffer pointer used to copy over the data
+ */
+void dev_uv_set_side(int pwm_duty, uint8_t dac_duty);
+
+/**
+ * @brief This function turns both UV LEDs off.
+ */
+void dev_uv_stop();
+
 
 # ifdef __cplusplus  
 }
