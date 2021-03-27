c76151b76f21be3a1e3c4c4b4cb48d8afcd55a34
Jerome <jvillapando@ne-rv.com>
2021-03-27 14:49:30 -0400

    Imported dev avr sensor

---
+++ Firmware/AVR/AVR_SENSOR/include/pinConfig.h
@@ -0,0 +1,51 @@
+/**
+ * @file pinConfig.h
+ * @author Tsugumi Murata (tmurata293)
+ * @date 17 Feb 2021
+ * @brief Pin Config File for AVR DRIVER
+ *
+ */
+
+
+#ifndef _PINCONFIG_H_
+#define _PINCONFIG_H_
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#define PIN_DIR_INPUT       0
+#define PIN_DIR_OUTPUT      1
+
+#define RIGHT_COLLISION     PB0     // PCINT8
+#define LEFT_COLLISION      PB1     // PCINT9
+
+#define IR_FRONT_1          PA7
+#define IR_FRONT_2          PA6
+#define IR_RIGHT_1          PA4
+#define IR_RIGHT_2          PA5
+#define IR_LEFT_1           PA0
+#define IR_LEFT_2           PA3
+
+#define IR_FRONT_1_MUX      0b00000111  //ADC7
+#define IR_FRONT_2_MUX      0b00000110  //ADC6
+#define IR_RIGHT_1_MUX      0b00000100  //ADC4  
+#define IR_RIGHT_2_MUX      0b00000101  //ADC5
+#define IR_LEFT_1_MUX       0b00000000  //ADC0
+#define IR_LEFT_2_MUX       0b00000011  //ADC3
+
+#define UART_TX             PA1
+#define TX_PORT             PORTA
+#define TX_DDR              DDRA
+#define TX_DDR_PIN          DDA1                      
+
+#define UART_RX             PA2 
+
+#ifdef __cplusplus  
+}
+#endif 
+
+
+#endif 
---
+++ Firmware/AVR/AVR_SENSOR/lib/uart_comm/uart_attiny.h
@@ -0,0 +1,26 @@
+/**
+ * @file uart_attiny.h
+ * @author Jerome Villapando
+ * @date 15 Feb 2021
+ * @brief Device configure header files
+ *
+ * This document will contains device configure content
+ */
+#ifndef UART_ATTINY_H
+#define UART_ATTINY_H
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+#include "../../include/pinConfig.h"
+
+
+void uart_attiny_init(void);
+void UART_tx(char character);
+void UART_tx_str(char* string, uint8_t len);
+void uart_test_code(void);
+
+
+#endif //UART_ATTINY_H
